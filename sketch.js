4fa78e7d5bd50833d6741de697ec9417359775c2
Hawer-tow <TNTHAWER1@GMAIL.COM>
2026-02-11 10:36:16 +0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,61 @@
+// Declare global variables
+let song;
+let vid;
+let angle = 0;
+
+function preload() {
+  // Load a sound file (replace with your own file path or URL)
+  song = loadSound('assets/alex_warren_ordinary_lyrics_aac_44129.m4a');
+  
+  // Load a video file (replace with your own file path or URL)
+  vid = createVideo('assets/4a7a49d9a8dbde8ae31a94ea856c3356[1].mp4');
+  vid.hide(); // Hide default video player
+}
+
+function setup() {
+  createCanvas(800, 600);
+  textAlign(CENTER, CENTER);
+  textSize(20);
+}
+
+function draw() {
+  background(220);
+
+  // Display video in the background
+  image(vid, 0, 0, width, height);
+
+  // Animated rotating shape overlay
+  push();
+  translate(width/2, height/2);
+  rotate(angle);
+  fill(255, 100, 150, 150);
+  rectMode(CENTER);
+  rect(0, 0, 200, 200);
+  pop();
+
+  angle += 0.01;
+
+  // Instructions
+  fill(255);
+  text("Press 'P' to Play/Pause Music | Press 'V' to Play/Pause Video", width/2, height - 30);
+}
+
+function keyPressed() {
+  // Toggle music
+  if (key === 'P') {
+    if (song.isPlaying()) {
+      song.pause();
+    } else {
+      song.play();
+    }
+  }
+
+  // Toggle video
+  if (key === 'V') {
+    if (vid.elt.paused) {
+      vid.play();
+    } else {
+      vid.pause();
+    }
+  }
+}
