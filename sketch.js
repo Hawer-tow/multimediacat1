ae05f6b7d4cfa6ee10b78770245d3d77abb62e5a
Hawer-tow <TNTHAWER1@GMAIL.COM>
2026-02-12 18:18:37 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,13 @@
-// Declare global variables
 let song;
 let vid;
 let angle = 0;
 let videoPlaying = false;
+let audioPlaying = false;
 
 function preload() {
-  // Load a sound file
   song = loadSound('assets/alex_warren_ordinary_lyrics_aac_44129.m4a');
-
-  // Load a video file
   vid = createVideo('assets/4a7a49d9a8dbde8ae31a94ea856c3356[1].mp4');
-  vid.hide(); // hide default video player
+  vid.hide();
 }
 
 function setup() {
@@ -28,10 +25,10 @@ function setup() {
 function draw() {
   background(220);
 
-  // Display video in the background
+  // Draw video
   image(vid, 0, 0, width, height);
 
-  // Animated rotating shape overlay
+  // Rotating square overlay
   push();
   translate(width/2, height/2);
   rotate(angle);
@@ -42,22 +39,21 @@ function draw() {
 
   angle += 0.01;
 
-  // Instructions
-  fill(0);
+  fill(255);
   text("Press 'P' to Play/Pause Music | Press 'V' to Play/Pause Video", width/2, height - 30);
 }
 
 function keyPressed() {
-  // Toggle music
   if (key === 'P') {
-    if (song.isPlaying()) {
+    if (audioPlaying) {
       song.pause();
+      audioPlaying = false;
     } else {
       song.play();
+      audioPlaying = true;
     }
   }
 
-  // Toggle video
   if (key === 'V') {
     if (videoPlaying) {
       vid.pause();
@@ -69,10 +65,11 @@ function keyPressed() {
   }
 }
 
-// Unlock audio/video on first click (browser requirement)
+// Unlock audio/video on first click
 function mousePressed() {
-  if (!song.isPlaying()) {
+  if (!audioPlaying) {
     song.play();
+    audioPlaying = true;
   }
   if (!videoPlaying) {
     vid.play();
