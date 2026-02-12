a7c902fbb88a9a6c028776206ed6e53a78ed29c1
Hawer-tow <TNTHAWER1@GMAIL.COM>
2026-02-12 18:57:30 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
 let song;
 let vid;
-let angle = 0;
 let videoPlaying = false;
 let audioPlaying = false;
 
@@ -24,18 +23,10 @@ function setup() {
 function draw() {
   background(220);
 
+  // Draw video in background
   image(vid, 0, 0, width, height);
-/*
-  push();
-  translate(width / 2, height / 2);
-  rotate(angle);
-  fill(255, 100, 150, 150);
-  rectMode(CENTER);
-  rect(0, 0, 200, 200);
-  pop();
 
-  angle += 0.01;
-*/
+  // Instructions
   fill(0);
   text("Press 'P' to Play/Pause Music | Press 'V' to Play/Pause Video", width / 2, height - 30);
 }
@@ -63,7 +54,6 @@ function keyPressed() {
 }
 
 function mousePressed() {
-  // unlock audio/video on first click
   if (!audioPlaying) {
     song.play();
     audioPlaying = true;
