d795e13a62ad44e0ba8623761f9d7bf212e9675a
Hawer-tow <TNTHAWER1@GMAIL.COM>
2026-02-12 18:09:10 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,14 +2,15 @@
 let song;
 let vid;
 let angle = 0;
+let videoPlaying = false;
 
 function preload() {
-  // Load a sound file 
+  // Load a sound file
   song = loadSound('assets/alex_warren_ordinary_lyrics_aac_44129.m4a');
-  
- 
+
+  // Load a video file
   vid = createVideo('assets/4a7a49d9a8dbde8ae31a94ea856c3356[1].mp4');
-  vid.hide(); 
+  vid.hide(); // hide default video player
 }
 
 function setup() {
@@ -17,9 +18,11 @@ function setup() {
   textAlign(CENTER, CENTER);
   textSize(20);
 
-  vid.volume(0);  // mute so autoplay works
-  vid.loop();     // keep looping
-  vid.play();     // start playing right away
+  // Start video muted so autoplay works
+  vid.volume(0);
+  vid.loop();
+  vid.play();
+  videoPlaying = true;
 }
 
 function draw() {
@@ -40,7 +43,7 @@ function draw() {
   angle += 0.01;
 
   // Instructions
-  fill(255);
+  fill(0);
   text("Press 'P' to Play/Pause Music | Press 'V' to Play/Pause Video", width/2, height - 30);
 }
 
@@ -56,10 +59,23 @@ function keyPressed() {
 
   // Toggle video
   if (key === 'V') {
-    if (vid.elt.paused) {
-      vid.play();
-    } else {
+    if (videoPlaying) {
       vid.pause();
+      videoPlaying = false;
+    } else {
+      vid.play();
+      videoPlaying = true;
     }
+  }
+}
+
+// Unlock audio/video on first click (browser requirement)
+function mousePressed() {
+  if (!song.isPlaying()) {
+    song.play();
+  }
+  if (!videoPlaying) {
+    vid.play();
+    videoPlaying = true;
   }
 }
